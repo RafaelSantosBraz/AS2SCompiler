5a1bec667b2823381720a5bb5968be77321dfd30
RafaelSantosBraz <rafaelsantosbrazpfi@gmail.com>
2019-08-18 13:49:53 -0300

    Criation of a project for recognize Java code

---
+++ grammar tests/JavaGrammar/src/javagrammar/Run.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagrammar;
+
+import java.awt.HeadlessException;
+import java.io.IOException;
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import parser.*;
+
+/**
+ *
+ * @author Rafael Braz
+ */
+public class Run {
+
+    public static void main(String[] args) throws IOException {
+        String filename = "test.java";
+        CharStream stream = new ANTLRFileStream(filename);
+        JavaGrammarLexer lexer = new JavaGrammarLexer(stream);            //Lexer
+        TokenStream tokens = new CommonTokenStream(lexer);  //nextToken 
+        JavaGrammarParser parser = new JavaGrammarParser(tokens);         //Parser
+        JavaGrammarParser.CompilationUnitContext prog
+                = parser.compilationUnit();        //Exec Parser prog
+        //exibir(prog);
+        showParseTreeFrame(prog, parser);        
+    }
+
+    public static void exibir(ParseTree tree) {
+        for (int c = 0; c < tree.getChildCount(); c++) {
+            System.out.println(tree.getChild(c).getPayload());
+            exibir(tree.getChild(c));
+        }
+    }
+
+    private static void showParseTreeFrame(ParseTree tree, JavaGrammarParser parser) throws HeadlessException {
+        JFrame frame = new JFrame("SRC: " + tree.getText());
+        JPanel panel = new JPanel();
+        TreeViewer viewr = new TreeViewer(Arrays.asList(
+                parser.getRuleNames()), tree);
+        viewr.setScale(1);
+        panel.add(viewr);
+        frame.add(panel);
+        frame.setSize(1000, 600);
+        frame.setState(JFrame.MAXIMIZED_HORIZ);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ grammar tests/JavaGrammar/test.java
@@ -0,0 +1,9 @@
+package javagrammar;
+
+public class Run {
+
+    public static void main(String[] args) {
+            
+    }
+
+}
