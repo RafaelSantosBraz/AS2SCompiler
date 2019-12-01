9e2422a252514f7e6fec775a2836fae0667b0f22
RafaelSantosBraz <rafaelsantosbrazpfi@gmail.com>
2019-12-01 20:54:17 -0300

    correcting Java OrderedApp example and establishing jar beta version

--- StSCompiler/src/stscompiler/Run.java
+++ StSCompiler/src/stscompiler/Run.java
@@ -17,14 +17,15 @@ public class Run {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] params = new String[]{
-            "java",
-            "c",
-            "D:\\GitHub\\StS-Compilation-Framework\\runtime\\input",
-            "D:\\GitHub\\StS-Compilation-Framework\\runtime\\output",
-            "D:\\GitHub\\StS-Compilation-Framework\\runtime\\Tmaps\\Java_CST_eCST.tmap"
-        };
-        ProcessControl.execute(params);        
+//        String[] params = new String[]{
+//            "java",
+//            "c",
+//            "D:\\GitHub\\StS-Compilation-Framework\\runtime\\input",
+//            "D:\\GitHub\\StS-Compilation-Framework\\runtime\\output",
+//            "D:\\GitHub\\StS-Compilation-Framework\\runtime\\Tmaps\\Java_CST_eCST.tmap"
+//        };
+//        ProcessControl.execute(params);        
+        ProcessControl.execute(args);
     }
 
 }
--- runtime/examples/Java_Code/OrderedArray/OrderedApp.java
+++ runtime/examples/Java_Code/OrderedArray/OrderedApp.java
@@ -5,11 +5,14 @@
 public class OrderedApp {
     public static void main(String[] args) {
         int maxSize = 100;
+        int[] vet = new int[maxSize];
         OrdArray arr;
-        arr = new OrdArray(maxSize);
-        arr.insert(77);
-        arr.insert(99);
+        arr = new OrdArray();
+        arr.insert(77, vet);
+        arr.insert(99, vet);
         int searchKey = 55;
-        int p = arr.find(searchKey);
+        int p = arr.find(searchKey, vet);
+        System.out.printf("%d\n", p);
+        arr.display(vet);
     }
 }
